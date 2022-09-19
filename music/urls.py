3ac8a57e5717cf87f8546cf6e32175818349c339
unknown <bensandlin2@gmail.com>
2022-09-19 16:09:18 -0500

    Added in the search by ID function.

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.music_list),
-    path('music/<int:pk>/', views.music_list)
+    path('<int:pk>/', views.music_detail)
 ]
--- music/views.py
+++ music/views.py
@@ -11,10 +11,19 @@ def music_list(request):
         music = Music.objects.all()
         serializer = MusicSerializer(music, many=True)
         return Response(serializer.data)
-
-
     elif request.method == 'POST':
         serializer = MusicSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def music_detail(request, pk):
+    try:
+        music = Music.objects.get(pk=pk)
+        serializer = MusicSerializer(music)
+        return Response(serializer.data)
+
+    except Music.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
