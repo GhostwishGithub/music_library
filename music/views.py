ccc3f897aa94fb0b085f93cf2c42193259918cde
unknown <bensandlin2@gmail.com>
2022-09-19 14:53:40 -0500

    Added in the Get functionality and postman mirror.

--- music/views.py
+++ music/views.py
@@ -1,8 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import MusicSerializer
+from .models import Music
 
 @api_view(['GET'])
 def music_list(request):
+    music = Music.objects.all()
+
+    serializer = MusicSerializer(music, many=True)
 
 
-    return Response('ok')
+    return Response(serializer.data)
